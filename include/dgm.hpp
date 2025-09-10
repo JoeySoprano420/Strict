96ec1dc1b5806d394ba518eac607eb65ea698eb1
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-09-10 01:25:02 -0400

    Add function to emit NASM from DGM file

--- include/dgm.hpp
+++ include/dgm.hpp
@@ -4,3 +4,6 @@
 
 // Translate LLVM IR to DGM opcode stream
 void translateModuleToDGM(llvm::Module &M, const std::string &filename);
+
+// Emit NASM x64 assembly from DGM file
+void emitDGMtoNASM(const std::string &dgmFile, const std::string &nasmFile);
